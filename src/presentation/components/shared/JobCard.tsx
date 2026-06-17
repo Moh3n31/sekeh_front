fc16aba5d03b17c5cefae6a53a8cc9c73acdb9ad
moh3n31 <mohsenmahmudi79@gmail.com>
2026-06-17 09:50:27 +0330

    #dev
    - Applied some styles.

--- src/presentation/components/shared/JobCard.tsx
+++ src/presentation/components/shared/JobCard.tsx
@@ -12,7 +12,7 @@ export default function JobCard(props: JobCardProps) {
 	const [saved, setSaved] = useState(props.bookmarked ?? false);
 
 	return (
-		<div className="h-full w-full flex flex-col justify-between gap-2.5 rounded-lg border px-2.5 py-2 bg-surface border-border">
+		<div className="h-67 w-full flex flex-col justify-between gap-2.5 rounded-lg border px-2.5 py-2 bg-background border-border">
 			{showDetails ? (
 				<DetailsView
 					company_name={props.company_name}
@@ -29,11 +29,11 @@ export default function JobCard(props: JobCardProps) {
 				/>
 			)}
 
-			<menu className="flex items-center justify-between gap-4 h-9">
+			<menu className="flex items-center justify-between gap-4 h-9 text-[12px]">
 				<button
 					onClick={() => setShowDetails((prev) => !prev)}
 					className="cursor-pointer w-full h-full rounded-full text-background bg-primary-action transition">
-					{showDetails ? "Go Back" : "View Details"}
+					{showDetails ? "Back" : "Details"}
 				</button>
 
 				<button
@@ -41,15 +41,13 @@ export default function JobCard(props: JobCardProps) {
 						window.open(props.job_url, "_blank", "noopener,noreferrer")
 					}
 					className="cursor-pointer w-full h-full rounded-full border-2 text-primary-action bg-transparent transition">
-					Open Link
+					Open
 				</button>
 
-				<button onClick={() => setSaved((prev) => !prev)}>
+				<button onClick={() => setSaved((prev) => !prev)} className="cursor-pointer">
 					<BookmarkIcon
-						className={`size-6 ${
-							saved
-								? "[&>g>path]:fill-primary-action [&>g>path]:stroke-surface"
-								: "[&>g>path]:stroke-primary-action"
+						className={`size-6 [&>g>path]:stroke-primary-action ${
+							saved && "[&>g>path]:fill-primary-action/60"
 						}`}
 					/>
 				</button>
@@ -70,26 +68,25 @@ function DetailsView({
 	company_reviews,
 }: DetailsViewProps) {
 	return (
-		<div className="flex flex-col gap-1.5 h-full">
+		<div className="flex flex-col h-full justify-start gap-2.5 text-primary-text text-[14px]">
 			<h3 className="text-[16px] font-medium text-text-muted">Details:</h3>
-			<div className="flex flex-col gap-2.5 text-base text-primary-text text-[14px]">
-				<div className="flex gap-3">
-					<span className="text-text-muted">Company:</span>
-					<span className="font-semibold">{company_name}</span>
-				</div>
-				<div className="flex gap-3">
-					<span className="text-text-muted">Source:</span>
-					<span className="font-semibold">{source_site}</span>
-				</div>
-				<div className="flex gap-3">
-					<span className="text-text-muted">Reviews:</span>
-					<span className="font-semibold">
-						{company_reviews.score.toFixed(1)} ⭐{" "}
-						<span className="text-text-muted font-normal">
-							({company_reviews.count} reviews)
-						</span>
+
+			<div className="flex gap-3">
+				<span className="text-text-muted">Company:</span>
+				<span className="font-semibold">{company_name}</span>
+			</div>
+			<div className="flex gap-3">
+				<span className="text-text-muted">Source:</span>
+				<span className="font-semibold">{source_site}</span>
+			</div>
+			<div className="flex gap-3">
+				<span className="text-text-muted">Reviews:</span>
+				<span className="font-semibold">
+					{company_reviews.score.toFixed(1)} ⭐{" "}
+					<span className="text-text-muted font-normal">
+						({company_reviews.count} reviews)
 					</span>
-				</div>
+				</span>
 			</div>
 		</div>
 	);
@@ -111,22 +108,22 @@ function OverView({
 	source_site,
 }: OverViewProps) {
 	return (
-		<div className="flex flex-col h-full justify-between gap-2.5">
+		<div className="flex flex-col h-full justify-start gap-2.5">
 			<div className="flex items-center gap-4">
-				<div className="w-20">
+				<div className="w-16">
 					<MatchCircle percentage={match_percent} />
 				</div>
 				<div className="flex flex-col gap-0.5">
 					<h2 className="text-[16px] font-bold text-primary-text">{title}</h2>
-					<p className="text-base text-text-muted">
+					<p className="text-[14px] text-text-muted">
 						{company_name} · {source_site}
 					</p>
 				</div>
 			</div>
 
-			<div>
-				<p className="text-base text-text-muted text-[14px]">Requirements:</p>
-				<ul className="list-disc flex flex-col gap-1 pl-8 text-[14px] text-primary-text ">
+			<div className="h-full">
+				<p className="text-text-muted text-[14px]">Requirements:</p>
+				<ul className="list-disc flex flex-col gap-1 pl-8 text-[14px] max-h-24 text-primary-text overflow-y-auto">
 					{requirements.map((requirement) => (
 						<li key={requirement}>{requirement}</li>
 					))}
