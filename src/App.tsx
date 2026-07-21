7eeef31c5da567d86d2a0b8dd05f1bd42741dbff
moh3n31 <mohsenmahmudi79@gmail.com>
2026-07-21 10:12:51 +0330

    Modified page cofigs.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ToastContainer from "./presentation/components/shared/ToastContainer";
 export default function App() {
 	const queryClient = new QueryClient();
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
 			<QueryClientProvider client={queryClient}>
 				<div dir="rtl">
 					<AllRoutes />
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-	base: "/sekeh_front/",
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		base: env.VITE_BASEـFRONT_URL,
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
+	};
 });
